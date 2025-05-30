66f7b17c3b0dde1bc15e353ccb9636bd0bdcb65e
HanifFauzan-n <haniffauzann55@gmail.com>
2025-05-31 00:02:54 +0700

    Add select sort

--- src/learning/Sorting.java
+++ src/learning/Sorting.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Sorting {
 
+    // Bubble sort
     public void sortingFunction(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -15,9 +16,26 @@ public class Sorting {
                 }
             }
         }
-            System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
 
     }
-    
+
+    // Selecting sort
+    public void selectingSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println(Arrays.toString(arr));
+            int minIndx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndx]) {
+                    minIndx = j;
+                }
+            }
+            int tamp = arr[minIndx];
+            arr[minIndx] = arr[i];
+            arr[i] = tamp;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
 
 }
