50905cc43434475fbf6138f9256374a2d3b82bd8
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-01 22:31:16 +0700

    test

--- src/App.java
+++ src/App.java
@@ -7,7 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Sorting sorting = new Sorting();
-        int[] data = { 5, 2, 9, 1, 5, 6 };
-        sorting.mergeSort(data);
+        int[] data = { 15, 32, 29, 71, 47, 106 };
+        sorting.quickSort(data, 0, data.length - 1);
+
     }
 }
