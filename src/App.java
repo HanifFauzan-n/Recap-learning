0eed368adbb57d2a4fdcfb12aa82d1190a42e423
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-02 20:20:03 +0700

    Add Recursion reverse

--- src/App.java
+++ src/App.java
@@ -1,14 +1,25 @@
 package src;
 
-import src.learning.Sorting;
+// import src.learning.Sorting;
+
+import src.learning.Recursion;
+
 
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Sorting sorting = new Sorting();
-        int[] data = { 15, 32, 29, 71, 47, 106 };
-        sorting.quickSort(data, 0, data.length - 1);
 
+        // Sorting
+        // Sorting sorting = new Sorting();
+        // int[] data = { 15, 32, 29, 71, 47, 106 };
+        // sorting.quickSort(data, 0, data.length - 1);
+
+        // Recursion
+        Recursion recursion = new Recursion();
+        long hasil = recursion.recursionFibonacci(6);
+        System.out.println(hasil);
+        String output = recursion.recursionReverse("Fauzan");
+        System.out.println(output);
     }
 }
--- src/learning/Recursion.java
+++ src/learning/Recursion.java
@@ -9,11 +9,17 @@ public class Recursion {
         return n * recursionFactorial(n - 1);
     }
 
-    public int recursionFibonacci(int n){
-        if (n < 1) {
+    public long recursionFibonacci(int n){
+        if (n <= 1) {
             return n;
         }
         return  recursionFibonacci(n - 1) + recursionFibonacci(n - 2);
+    }
+
+    public String recursionReverse(String data){
+        if(data.isEmpty()) return data;
+
+        return recursionReverse(data.substring(1)) + data.charAt(0);
     }
     
 }
